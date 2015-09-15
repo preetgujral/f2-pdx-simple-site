ca0b829ecc6d1689b019d17f81285763a041b9c5
PREET GUJRAL <PREET@PREETGUJRAL.COM>
2015-09-14 19:29:23 -0700

    replacing 3 main files with beth's version

--- server.js
+++ server.js
@@ -2,75 +2,98 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
 
-//3000 is a common port for testing
-//process is a default. PORT is a constant. essentially use default OR 3000
+// TODO: Install the body parser module in your terminal:
+//
+// npm install --save body-parser
+//
+// (Make sure you're in the root directory of your f2-pdx-simple-site project!)
 
-app.listen(port, function() {
-  console.log('server started on port ' + port);
-});
+// Then uncomment the following lines and fill in the question marks.
+// Hint: the module name is: body-parser
+// var bodyparser = require("??????");
+// app.use(bodyparser.json());
+// app.use(bodyparser.urlencoded({extended: true}));
 
-//slide 32
-// var array = ['string1', 'string2', 'string3'];
-
-// app.get("/preetgujral", function (req, res) {
-//   var randomIndex = Math.floor(Math.random()*array.length);
-//   res.send(array[randomIndex]);
-// });
-
-//35 - send object data - when user hits /preetgujral they get a random lecture
-
+app.use(express.static(__dirname + '/app/'));
 
 var course = {
-  'courseLevel': '201',
-  'name': 'Foundations 2: JavaScript',
-  'lectures': [
-    {name: 'class1Lecture', topic: 'Paperwork, Setup & Javascript Basics (Part 1)'},
-    {name: 'class2Lecture', topic: 'JavaScript Basics (Part 2) & Intro to Node'},
-    {name: 'class3Lecture', topic: 'Array Methods, Functions & Scope/Hoisting'},
-    {name: 'class4Lecture', topic: 'OOP in JavaScript and Classes/Inheritance'},
-    {name: 'class5Lecture', topic: 'JavaScript in the Browser & Intro to jQuery'},
-    {name: 'class6Lecture', topic: 'Building a Server & Using Ajax'},
-    {name: 'class7Lecture', topic: 'Build & Deploy an App'},
-    {name: 'class8Lecture', topic: 'Functional Programming with lodash'}
+  level: 201,
+  name: 'Foundations 2: JavaScript',
+  lectures: [
+    { "topic": "Intro, Basics 1",
+      "notes": [ "The primitive data types are Boolean, Null, Undefined, Number, and String.",
+                 "Parameters become variables inside their function.",
+                 "A method is a function that is also the value of an object property."
+               ]
+    },
+    { "topic": "Basics 2",
+      "notes": [ "Logical operators return the result of the last expression evaluated.",
+                 "Loops execute the same instructions multiple times.",
+                 "An infinite loop is a loop that never satisfies its exit condition."
+               ]
+    },
+    { "topic": "Array Methods, Functions, Scope",
+      "notes": [ "join() takes a delimiter, returns a string.",
+                 "Generally useful code is often packaged up as a collection of functions and data in libraries.",
+                 "JavaScript is function-scoped."
+               ] },
+    { "topic": "Object Oriented Programming" },
+    { "topic": "JavaScript in the Browser" },
+    { "topic": "Node.js and Express" },
+    { "topic": "Workshop" },
+    { "topic": "lodash" }
+  ],
+  labs: [
+    { "topic": "Basics" },
+    { "topic": "Object Oriented Programming"},
+    { "topic": "Project"}
   ]
-  ,
-  'labs': [
-    {name: 'class1Lab', topic: 'labtopic1'},
-    {name: 'class2Lab', topic: 'labtopic2'},
-    {name: 'class3Lab', topic: 'labtopic3'},
-    {name: 'class4Lab', topic: 'labtopic4'},
-    {name: 'class5Lab', topic: 'labtopic5'},
-    {name: 'class6Lab', topic: 'labtopic6'},
-    {name: 'class7Lab', topic: 'labtopic7'}
-  ]
+};
+
+function findPhrase(phrase) {
+  var results = {
+    lectures: [],
+    labs: []
+  };
+
+  // Let's start by stubbing out some functionality so we can test
+  // the other parts of our code before writing the search algorithm.
+  results.lectures = course.lectures;
+  results.labs = course.labs;
+
+  return results;
 }
 
+// TODO: Instead of app.get(), we want to follow the same pattern using app.post().
+//       Surround the following lines of code with app.post() and make sure to
+//       call your endpoint "search".
 
-//40 - now send an index.html
-app.use(express.static(__dirname + "/"));
 
-app.get("/", function (req, res) {
+// TODO: Once you've surrounded this code with app.post(),
+//       we need to unpack our search phrase from req.body.
+//       Instead of the question marks, specify the name of the
+//       parameter that contains our search text.
+// var searchResults = findPhrase(req.body.?????);
+// res.json(searchResults);
+
+
+
+app.get('/lectures', function (req, res) {
+  res.json(course.lectures);
+});
+
+app.get('/labs', function (req, res) {
+  res.json(course.labs);
+});
+
+app.get('/course_name', function (req, res) {
+  res.json(course.name);
+});
+
+app.get('/', function (req, res) {
   res.sendFile();
 });
 
-app.get("/lectures", function (req, res) {
-  var randomIndex = Math.floor(Math.random()*course.lectures.length);
-  res.json(course.lectures[randomIndex]);
+app.listen(port, function () {
+  console.log('server started on port ' + port);
 });
-
-
-
-
-
-
-
-// app.get("/f2coursework", function (req, res) {
-//   res.send(course.courseLevel);
-// });
-
-//res has send method built in
-
-// app.get("/quote", function (req, res) {
-//   var randomIndex = Math.floor(Math.random()*course.lectures.length);
-//     res.send(course.lectures[randomIndex].name);
-// });
