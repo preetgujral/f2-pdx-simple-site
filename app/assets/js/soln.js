0bde72e3d600a42d468105e5f5c83a5701180c5d
PREET GUJRAL <PREET@PREETGUJRAL.COM>
2015-09-25 16:19:51 -0700

    main functionalities work, looks ugly, needs to be cleaned up and run grunt

--- app/assets/js/soln.js
+++ app/assets/js/soln.js
@@ -1,13 +1,13 @@
-//this is where to put the client side get requests.
+//this is where to put the client side http requests.
 
 $(function () { //when page is loaded
 //this will happen on get-info click
-  $('.get-info').click(function () {
-    var section = $(this).attr('id');
-    $.get("http://localhost:3000/" + section, function (response) {
-      $("#show-" + section).text(response[0].topic);
-    });
-  });
+  // $('.get-info').click(function () {
+  //   var section = $(this).attr('id');
+  //   $.get("http://localhost:3000/" + section, function (response) {
+  //     $("#show-" + section).text(response[0].topic);
+  //   });
+  // });
 //this is where you put in what will happen on button click
   // Code Challenge 1:
   // When the user clicks the "Find It" button,
@@ -17,11 +17,17 @@ $(function () { //when page is loaded
 
     var searchParam = $('input[name=searchText]').val();
     var lucky = $('input[name=lucky]').is(':checked');
+    console.log(lucky)
     var data = {searchText: searchParam, lucky: lucky};
     var url = 'http://localhost:3000/search';
 
     console.log(data); // this will be a sanity check to see that we're getting the data
+
     $.post(url, data, function(response) {
+      if (response === "nothing here") {
+        $("#notfound").text("Search Not found")
+      } else
+    $("#notfound").text("");
       var lectures,
           output = '';
 
@@ -35,6 +41,7 @@ $(function () { //when page is loaded
       //   }
       // }
 
+// would this be an appropriate place to put an if (output is blank) {return error) else
       if (response.notes && response.notes.constructor === Array) {
         for (var i = 0; i < response.notes.length; i++) {
           output += '<li>' + response.notes[i] + '</li>';
@@ -56,15 +63,17 @@ $(function(){
     console.log ("Button clicked on: " + buttonClicked);
     msg = "Results:"
     $.get("http://localhost:3000/" + buttonClicked, function(response) {
-      $.each(response, function() {
-        $.each(this, function(k, v) {
-          msg += "<br>" + v
+      if (buttonClicked === "getlucky") {
+        msg = "<br>" + response;
+        $("#" + "show-"+buttonClicked).html(msg);
+      } else {
+        $.each(response, function() {
+          $.each(this, function(k, v) {
+            msg += "<br>" + v
+          });
         });
-      });
-
-      $("#" + "show-"+buttonClicked)
-      .html(msg);
-
-    });
+        $("#" + "show-"+buttonClicked).html(msg);
+      };
+    })
   })
 })
--- server4.js
+++ server4.js
@@ -2,7 +2,6 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
 
-//what is bodyparser doing? would help understand var searchResults =
 var bodyparser = require("body-parser");
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({extended: true}));
@@ -10,13 +9,26 @@ app.use(bodyparser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/app/'));
 
 var f2search = require("./lib/f2search");
-
+// console.log(f2search("Hi"));
 app.post('/search', function (req, res) {
-  var searchResults = f2search(req.body.searchText);
-  res.json(searchResults);
+ //if im feeling lucky box is checked
+  //console.log(typeof req.body.lucky)
+  //console.log (req.body.lucky)//if (gettingLucky==="true") {
+  //  console.log("this validates that it registered as true");//select a random entry from the "notes" options and display it immediately.
+  if (req.body.lucky==="true") {
+    return "unselect I feel lucky box"
+  }
+
+  //TODO - else if (no results were found) {return an informative message to the user}
+  else {
+//   return search results
+    var searchResults = f2search(req.body.searchText);
+    //console.log(searchResults)
+    res.json(searchResults);
+  //}
+  }
 });
 
-//this sends all lectures to /lectures. test by spinning up and going to this site. which i did and it returned an error - course is not defined
 
 app.get('/lectures', function (req, res) {
   res.json(course.lectures)
@@ -25,6 +37,38 @@ app.get('/lectures', function (req, res) {
 app.get('/labs', function (req, res) {
   res.json(course.labs);
 });
+
+app.get('/getlucky', function (req, res) {
+//this is where you get a random note and res.json it
+  //res.json(["string1","string2","string3"])
+
+  var temp = []
+  var courseLectures = course.lectures
+  courseLectures.forEach(function (index, val){
+    temp = temp.concat(index.notes);
+    })
+    console.log(temp)
+    randomIndex = Math.floor(Math.random()*temp.length)
+    //randomIndexofArrayInTemp = Math.floor(Math.random()*temp[randomIndexofTemp])
+    //randomNote = temp[randomIndexofNote][randomIndexofArrayInNote]
+    console.log(randomIndex)
+  res.json(temp[randomIndex])
+    //console.log("indexoftemp" + randomIndexofTemp)
+    //= (array) {
+      //Math.floor(Math.random()*array.length)
+      // }
+    //randomNote = randomIndex(randomIndex(temp));
+    //console.log(randomNote)
+
+    //temp.forEach( function (index){
+      //console.log(temp[index])
+    //  lucky+=temp[index]
+
+  //console.log(lucky)
+  })
+  //var randomIndex = Math.floor(Math.random()*lucky.length);
+
+//});
 
 app.get('/course_name', function (req, res) {
   res.json(course.name);
@@ -72,11 +116,16 @@ var course = {
                    "Generally useful code is often packaged up as a collection of functions and data in libraries.",
                    "JavaScript is function-scoped."
                  ] },
-      { "topic": "Object Oriented Programming" },
-      { "topic": "JavaScript in the Browser" },
-      { "topic": "Node.js and Express" },
-      { "topic": "Workshop" },
-      { "topic": "lodash" }
+      { "topic": "Object Oriented Programming",
+        "notes": [ "OOP-blank note"]},
+      { "topic": "JavaScript in the Browser",
+        "notes": [ "JSinB-blank note"]},
+      { "topic": "Node.js and Express",
+        "notes": [ "node.js-blank note"]},
+      { "topic": "Workshop",
+        "notes": ["workshop-blank note"]},
+      { "topic": "lodash",
+        "notes": [ "lodash-blank note"]}
     ],
     labs: [
       { "topic": "Basics" },
@@ -84,3 +133,7 @@ var course = {
       { "topic": "Project"}
     ]
   };
+
+
+//1st route - duplicate data in both modules
+//2nd - what i did
