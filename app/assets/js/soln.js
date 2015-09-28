1e2624358e879671a7b556bb0c3bf356804db4c0
PREET GUJRAL <PREET@PREETGUJRAL.COM>
2015-09-28 16:59:16 -0700

    everything completed. just need to grunt, deploy and submit

--- app/assets/js/soln.js
+++ app/assets/js/soln.js
@@ -1,13 +1,6 @@
 //this is where to put the client side http requests.
 
-$(function () { //when page is loaded
-//this will happen on get-info click
-  // $('.get-info').click(function () {
-  //   var section = $(this).attr('id');
-  //   $.get("http://localhost:3000/" + section, function (response) {
-  //     $("#show-" + section).text(response[0].topic);
-  //   });
-  // });
+$(function () {
 //this is where you put in what will happen on button click
   // Code Challenge 1:
   // When the user clicks the "Find It" button,
@@ -17,11 +10,11 @@ $(function () { //when page is loaded
 
     var searchParam = $('input[name=searchText]').val();
     var lucky = $('input[name=lucky]').is(':checked');
-    console.log(lucky)
+    //console.log(lucky)
     var data = {searchText: searchParam, lucky: lucky};
     var url = 'http://localhost:3000/search';
 
-    console.log(data); // this will be a sanity check to see that we're getting the data
+    //console.log(data); // this will be a sanity check to see that we're getting the data
 
     $.post(url, data, function(response) {
       if (response === "nothing here") {
@@ -30,30 +23,15 @@ $(function () { //when page is loaded
     $("#notfound").text("");
       var lectures,
           output = '';
-
-      // lectures = response.lectures;
-
-      // for (var i = 0; i < lectures.length; i++) {
-      //   if (lectures[i].notes && lectures[i].notes.constructor === Array) {
-      //     for (var j = 0; j < lectures[i].notes.length; j++) {
-      //       output += '<li>' + lectures[i].notes[j] + '</li>';
-      //     }
-      //   }
-      // }
-
-// would this be an appropriate place to put an if (output is blank) {return error) else
       if (response.notes && response.notes.constructor === Array) {
         for (var i = 0; i < response.notes.length; i++) {
           output += '<li>' + response.notes[i] + '</li>';
         }
       }
-
       output = '<ul>' + output + '</ul>';
-
       $("#search-results").html(output);
     });
   });
-
 });
 
 //on click, retrieve relevant fragment of JSON data. loop through that data and output
--- server4.js
+++ server4.js
@@ -11,24 +11,9 @@ app.use(express.static(__dirname + '/app/'));
 var f2search = require("./lib/f2search");
 // console.log(f2search("Hi"));
 app.post('/search', function (req, res) {
- //if im feeling lucky box is checked
-  //console.log(typeof req.body.lucky)
-  //console.log (req.body.lucky)//if (gettingLucky==="true") {
-  //  console.log("this validates that it registered as true");//select a random entry from the "notes" options and display it immediately.
-  if (req.body.lucky==="true") {
-    return "unselect I feel lucky box"
-  }
-
-  //TODO - else if (no results were found) {return an informative message to the user}
-  else {
-//   return search results
-    var searchResults = f2search(req.body.searchText);
-    //console.log(searchResults)
-    res.json(searchResults);
-  //}
-  }
+  var searchResults = f2search(req.body.searchText);
+  res.json(searchResults);
 });
-
 
 app.get('/lectures', function (req, res) {
   res.json(course.lectures)
@@ -38,37 +23,11 @@ app.get('/labs', function (req, res) {
   res.json(course.labs);
 });
 
+var lablectbutton = require("./lib/lablectbutton");
 app.get('/getlucky', function (req, res) {
-//this is where you get a random note and res.json it
-  //res.json(["string1","string2","string3"])
-
-  var temp = []
-  var courseLectures = course.lectures
-  courseLectures.forEach(function (index, val){
-    temp = temp.concat(index.notes);
-    })
-    console.log(temp)
-    randomIndex = Math.floor(Math.random()*temp.length)
-    //randomIndexofArrayInTemp = Math.floor(Math.random()*temp[randomIndexofTemp])
-    //randomNote = temp[randomIndexofNote][randomIndexofArrayInNote]
-    console.log(randomIndex)
-  res.json(temp[randomIndex])
-    //console.log("indexoftemp" + randomIndexofTemp)
-    //= (array) {
-      //Math.floor(Math.random()*array.length)
-      // }
-    //randomNote = randomIndex(randomIndex(temp));
-    //console.log(randomNote)
-
-    //temp.forEach( function (index){
-      //console.log(temp[index])
-    //  lucky+=temp[index]
-
-  //console.log(lucky)
-  })
-  //var randomIndex = Math.floor(Math.random()*lucky.length);
-
-//});
+  var output = lablectbutton(req.body.output);
+  res.json(output);
+})
 
 app.get('/course_name', function (req, res) {
   res.json(course.name);
@@ -81,21 +40,8 @@ app.get('/', function (req, res) {
 app.listen(port, function () {
   console.log('server started on port ' + port);
 });
-//this is the code from server.js
-//remember that you just copied this from github.com, so do not copy paste anything from here
-//remember to comment out in order to test
 
-// app.get("/lectures", function (req, res) {
-//   var randomIndex = Math.floor(Math.random()*course.lectures.length);
-//   res.json(course.lectures[randomIndex]);
-// });
-
-// app.get("/labs", function (req, res) {
-//   var randomIndex = Math.floor(Math.random()*course.labs.length);
-//   res.json(course.labs[randomIndex]);
-// });
-
-var course = {
+  var course = {
     level: 201,
     name: 'Foundations 2: JavaScript',
     lectures: [
@@ -133,7 +79,3 @@ var course = {
       { "topic": "Project"}
     ]
   };
-
-
-//1st route - duplicate data in both modules
-//2nd - what i did
